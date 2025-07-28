19bd8b3e8ed93a28fa784cb48d778ec23ca6a3ed
balint1414 <ropoli.balint.1414@gmail.com>
2025-07-28 21:05:55 +0200

    Added setup command

--- main.py
+++ main.py
@@ -5,11 +5,22 @@ load_dotenv()
 bot = discord.Bot()
 db = sqlite3.connect("db.db")
 owner_ids = [int(id) for id in os.getenv("ADMIN_IDS", "").split(",") if id.isdigit()]
-noPermEmbed = discord.Embed(title="No permission", description="You don't have permission to use this command.", color=discord.Color.red())
-needToSetup = discord.Embed(title="Invalid token", description="You have not set a valid Pterodactyl token yet. To set the token, use the /setup command", color=discord.Color.red())
+setup_command_id = None
 @bot.event
 async def on_ready():
+    global setup_command_id
     print(f"Started running on {len(bot.guilds)} servers.")
+    for cmd in bot.application_commands:
+        if cmd.name == "setup":
+            setup_command_id = cmd.id
+
+def need_to_setup_embed():
+    return discord.Embed(
+        title="Invalid token",
+        description=f"You have not set a valid Pterodactyl token yet. To set the token, use the </setup:{setup_command_id}> command",
+        color=discord.Color.red()
+    )
+noPermEmbed = discord.Embed(title="No permission", description="You don't have permission to use this command.", color=discord.Color.red())
 
 list = bot.create_group("list", "Fetch datas from Pterodactyl")
 
@@ -20,7 +31,7 @@ async def users(ctx):
         await ctx.respond(embed=noPermEmbed, ephemeral=True)
         return
     try:
-        r = requests.get(os.getenv("PTERODACTYL_URL") + "/api/application/users", headers={"Authorization":"Bearer " + os.getenv("PTERODACTYL_ADMIN_API_KEY")})
+        r = requests.get(os.getenv("PTERODACTYL_URL") + "/api/application/users", headers={"Authorization":"Bearer " + os.getenv("PTERODACTYL_ADMIN_API_KEY"), "Accept": "Application/vnd.pterodactyl.v1+json"})
         embed = discord.Embed(title="Users", description="List of all users", color=discord.Color.green())
         for user in r.json()["data"]:
             embed.add_field(name=user["attributes"]["username"], value=f"ID: {user['attributes']['id']}\nEmail: {user['attributes']['email']}\nFull name: {user['attributes']['first_name']} {user['attributes']['last_name']}", inline=True)
@@ -38,15 +49,15 @@ async def servers(ctx):
         cur.execute("SELECT * FROM users WHERE userid = ?", (ctx.author.id,))
         user = cur.fetchone()
         if not user:
-            await ctx.respond(embed=needToSetup, ephemeral=True)
+            await ctx.respond(embed=need_to_setup_embed(), ephemeral=True)
             return
         else: 
             try:
                 token = user[1]
                 if not token:
-                    await ctx.respond(embed=needToSetup, ephemeral=True)
+                    await ctx.respond(embed=need_to_setup_embed(), ephemeral=True)
                     return
-                r = requests.get(os.getenv("PTERODACTYL_URL") + "/api/client", headers={"Authorization":"Bearer " + token})
+                r = requests.get(os.getenv("PTERODACTYL_URL") + "/api/client", headers={"Authorization":"Bearer " + token, "Accept": "Application/vnd.pterodactyl.v1+json"})
                 embed = discord.Embed(title="Servers", description=f"We found {len(r.json()['data'])} servers", color=discord.Color.green())
                 for server in r.json()["data"]:
                     embed.add_field(name=server["attributes"]["name"], value=f"```{server['attributes']['identifier']}```", inline=True)
@@ -57,7 +68,7 @@ async def servers(ctx):
                 await ctx.respond(embed=errorEmbed, ephemeral=True)
                 return
     try:
-        r = requests.get(os.getenv("PTERODACTYL_URL") + "/api/application/servers", headers={"Authorization":"Bearer " + os.getenv("PTERODACTYL_ADMIN_API_KEY")})
+        r = requests.get(os.getenv("PTERODACTYL_URL") + "/api/application/servers", headers={"Authorization":"Bearer " + os.getenv("PTERODACTYL_ADMIN_API_KEY"), "Accept": "Application/vnd.pterodactyl.v1+json"})
         embed = discord.Embed(title="Servers", description=f"We found {len(r.json()['data'])} servers", color=discord.Color.green())
         embed.set_footer(text="You are an admin, so you can see all servers.")
         for server in r.json()["data"]:
@@ -75,7 +86,7 @@ async def nodes(ctx):
         await ctx.respond(embed=noPermEmbed, ephemeral=True)
         return
     try:
-        r = requests.get(os.getenv("PTERODACTYL_URL") + "/api/application/nodes", headers={"Authorization":"Bearer " + os.getenv("PTERODACTYL_ADMIN_API_KEY")})
+        r = requests.get(os.getenv("PTERODACTYL_URL") + "/api/application/nodes", headers={"Authorization":"Bearer " + os.getenv("PTERODACTYL_ADMIN_API_KEY"), "Accept": "Application/vnd.pterodactyl.v1+json"})
         embed = discord.Embed(title="Nodes", description=f"We found {len(r.json()['data'])} nodes", color=discord.Color.green())
         for node in r.json()["data"]:
             embed.add_field(name=node["attributes"]["name"], value=f"```FQDN: {node['attributes']['fqdn']}\nMemory: {node['attributes']['allocated_resources']['memory']}/{node['attributes']['memory']} MiB\nDisk: {node['attributes']['allocated_resources']['disk']}/{node['attributes']['disk']} MiB```", inline=False)
@@ -85,5 +96,26 @@ async def nodes(ctx):
         await ctx.respond(embed=errorEmbed, ephemeral=True)
         return
 
+@bot.command(description="Setup your Pterodactyl token")
+async def setup(ctx, token: str):
+    await ctx.response.defer(ephemeral=True)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM users WHERE userid = ?", (ctx.author.id,))
+    user = cur.fetchone()
+    if user:
+        await ctx.respond(embed=discord.Embed(title="Error", description="You have already set up your Pterodactyl token.", color=discord.Color.red()), ephemeral=True)
+        return
+    try:
+        r = requests.get(os.getenv("PTERODACTYL_URL") + "/api/client", headers={"Authorization":"Bearer " + token, "Accept": "Application/vnd.pterodactyl.v1+json"})
+        if r.status_code == 200:
+            cur = db.cursor()
+            cur.execute("INSERT INTO users (userid, token) VALUES (?, ?)", (ctx.author.id, token))
+            db.commit()
+            await ctx.respond(embed=discord.Embed(title="Success", description="Your Pterodactyl token has been set up successfully.", color=discord.Color.green()), ephemeral=True)
+        else:
+            await ctx.respond(embed=discord.Embed(title="Error", description="You need to provide a valid token.", color=discord.Color.red()), ephemeral=True)
+    except Exception as e:
+        errorEmbed = discord.Embed(title="Error", description="An error occurred while processing your request. Maybe the token had a bad day?", color=discord.Color.red(), fields=[discord.EmbedField(name="Error", value=str(e))])
+        await ctx.respond(embed=errorEmbed, ephemeral=True)
 
 bot.run(os.getenv("TOKEN"))
